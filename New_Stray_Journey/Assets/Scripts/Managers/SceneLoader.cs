f0a54070103427d1997c0ba7bed553acd3eb12f6
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-01-09 22:40:30 -0300

    Title Screen

---
+++ New_Stray_Journey/Assets/Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    void Awake()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
