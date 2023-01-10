35f8357ad2d1537e4b9047e1f36325bfb896ebab
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-01-09 23:27:53 -0300

    Fixed TitleScreen & SceneLoader

--- New_Stray_Journey/Assets/Scripts/Managers/SceneLoader.cs
+++ New_Stray_Journey/Assets/Scripts/Managers/SceneLoader.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class SceneLoader : MonoBehaviour
 {
+	public static SceneLoader instance;
+	void Awake()
+	{
+		if (!instance)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
 
-    void Awake()
-    {
-        
-    }
+	public void ChangeScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
 
-   
-    void Update()
-    {
-        
-    }
+	public void ExitGame()
+	{
+		Application.Quit();
+#if UNITY_EDITOR
+		EditorApplication.ExitPlaymode();
+#endif
+	}
+
 }
