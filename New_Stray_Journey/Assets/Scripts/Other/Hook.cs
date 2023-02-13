528037e7fca9927a46818bde5ddf73c0e51f7433
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-02-13 01:30:51 -0300

    FK

--- New_Stray_Journey/Assets/Scripts/Other/Hook.cs
+++ New_Stray_Journey/Assets/Scripts/Other/Hook.cs
@@ -1,4 +1,48 @@
-﻿public class Hook : Bullet
+﻿using UnityEngine;
+using System.Collections;
+using Game.Player;
+using Game.Enemies;
+
+
+public class Hook : Bullet
 {
-	
+	[SerializeField] private bool _onShoot, _objectGrabbed;
+	[SerializeField] private PlayerHook p;
+
+	private void OnEnable()
+	{
+		_onShoot = true;
+	}
+
+	private void OnDisable()
+	{
+		_onShoot = false;
+	}
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.GetComponent<Interactable>() != null || collision.GetComponent<ActiveEnemy>() != null || 
+			collision.GetComponent<PassiveEnemy>() != null || collision.GetComponent<Collectable>() != null)
+		{
+			GrabEntity();
+		}
+	}
+
+	IEnumerator GetOverHere(GameObject objectToGrab)
+	{ 
+
+		while (_onShoot)
+		{
+			yield return null;
+			objectToGrab.transform.position = this.transform.position;
+		}
+
+		Debug.Log($"");
+	}
+
+	public void GrabEntity()
+	{
+		//var go  = 
+		_objectGrabbed = true;
+		//StartCoroutine(GetOverHere());
+	}
 }
