5abf60e238557c0b89fb1c46492affbf57d26ead
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-02-26 20:45:18 -0300

    WinScreen

---
+++ New_Stray_Journey/Assets/Scripts/Other/FinalVortex.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class FinalVortex : MonoBehaviour
+{
+	[SerializeField] private AudioSource _aSource;
+	[SerializeField] private AudioClip _vortexFinish, _winScreenMusic;
+	[SerializeField] private GameObject _winScreen;
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		collision.GetComponentInChildren<SpriteRenderer>().enabled = false;
+		StartCoroutine(WinTheGame());
+	}
+
+	IEnumerator WinTheGame()
+	{
+		_aSource.PlayOneShot(_vortexFinish);
+		
+		while (_aSource.isPlaying)
+		{
+		yield return null;
+
+		}
+		_winScreen.SetActive(true);
+		yield return null;
+		_aSource.PlayOneShot(_winScreenMusic);
+		
+	}
+}
